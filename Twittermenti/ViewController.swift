2d93ca450a9c90e464af51ec5978546704a0e9ae
Benjamin Salter <bennsalter@gmail.com>
2020-08-27 10:56:50 -0400

    initial commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Angela Yu on 17/07/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let tweetCount = 100
+    
+    private let sentimentClassifier = TweetSentimentClassifier()
+
+    private let swifter = Swifter(consumerKey: "1234567890", consumerSecret: "1234567890") // these are obviously fake
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+        fetchTweets()
+    }
+    
+    func fetchTweets() {
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: "en", count: tweetCount, tweetMode: .extended, success: { (results, metadata) in
+                var tweets = [TweetSentimentClassifierInput]()
+                for i in 0..<self.tweetCount {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                self.makePrediction(with: tweets)
+            }) { (error) in
+                print ("There was an error with the Twitter API request: \(error)")
+            }
+        }
+    }
+    
+    func makePrediction(with tweets: [TweetSentimentClassifierInput]) {
+        do {
+            var sentimentScore = 0
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+            for pred in predictions {
+                let sentiment = pred.label
+                if sentiment == "Neg" {
+                    sentimentScore -= 1
+                } else if sentiment == "Pos" {
+                    sentimentScore += 1
+                }
+            }
+            updateUI(with: sentimentScore)
+        } catch {
+            print("There was an error classifying the input: \(error)")
+        }
+    }
+    
+    func updateUI(with sentimentScore: Int) {
+        if sentimentScore > 20  {
+            self.sentimentLabel.text = "😍"
+        } else if sentimentScore > 10 {
+            self.sentimentLabel.text = "😀"
+        } else if sentimentScore > 0 {
+            self.sentimentLabel.text = "🙂"
+        } else if sentimentScore == 0 {
+            self.sentimentLabel.text = "😐"
+        } else if sentimentScore > -10 {
+            self.sentimentLabel.text = "😕"
+        } else if sentimentScore > -20 {
+            self.sentimentLabel.text = "😡"
+        } else {
+            self.sentimentLabel.text = "🤮"
+        }
+    }
+}
+
